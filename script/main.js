e87a8d4bb09daa2642b0770c6f4f26ff2afe9ba4
roman <roman.work@hotmail.com>
2023-10-10 16:26:09 +0300

    added dark mode

--- script/main.js
+++ script/main.js
@@ -7,3 +7,15 @@ document.addEventListener("click", (e) => {
     ? navmenu.classList.toggle("visible")
     : navmenu.classList.remove("visible");
 });
+
+let moon_icon = document.getElementById("moon_icon");
+
+moon_icon.onclick = function () {
+  document.body.classList.toggle("dark-theme");
+
+  if (document.body.classList.contains("dark-theme")) {
+    moon_icon.src = "../images/sunny-outline.png";
+  } else {
+    moon_icon.src = "../images/moon-outline.svg";
+  }
+};
